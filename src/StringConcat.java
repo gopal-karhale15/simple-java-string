110808ff62cdd427f334e1369096fcb60ba146af
gopal12-coder <b3848946@gmail.com>
2025-12-09 09:44:58 +0530

    feat: add program to print a simple string

---
+++ src/StringConcat.java
@@ -0,0 +1,10 @@
+public class StringConcat {
+    public static void main(String[] args) {
+        String first = "Hello";
+        String second = "Gopal_Karhale";
+
+        String result = first + " " + second;
+
+        System.out.println("Concatenated String: " + result);
+    }
+}
---
+++ src/StringLength.java
@@ -0,0 +1,8 @@
+public class StringLength {
+    public static void main(String[] args) {
+        String str = "Gopal Vijay Karhale";
+
+        System.out.println("String: " + str);
+        System.out.println("Length: " + str.length());
+    }
+}
